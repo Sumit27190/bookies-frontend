49fcd5fb2a01aa87b1ca430f10aa06809cda2176
Sumit27190 <sumitkapse27190@gmail.com>
2026-02-16 01:02:13 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
